fe78d24516f59a58ad15d7b2b42ccc5db4a28bce
JoseDiazCodes <josediazjr.a@gmail.com>
2024-11-06 10:37:20 -0500

    hw5

--- src/doubledispatch/TerrainExplorer.java
+++ src/doubledispatch/TerrainExplorer.java
@@ -3,17 +3,17 @@ package doubledispatch;
 public class TerrainExplorer implements ISpaceExplorer {
   @Override
   public void visit(Mercury mercury) {
-    SimulationBuilder.addToLog("Mapping Mercury");
+    SimulationBuilder.addToLog("Mapping: Mercury");
   }
 
   @Override
   public void visit(Mars mars) {
-    SimulationBuilder.addToLog("Mapping Mars");
+    SimulationBuilder.addToLog("Mapping: Mars");
   }
 
   @Override
   public void visit(Venus venus) {
-    SimulationBuilder.addToLog("Mapping Venus");
+    SimulationBuilder.addToLog("Mapping: Venus");
 
   }
 }
